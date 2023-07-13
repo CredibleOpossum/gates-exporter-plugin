d164dd80bf91d6c1e0377bffb9c18c290360c423
CredibleOpossum <CredibleOpossum@protonmail.com>
2023-07-13 17:23:14 -0400

    add error message for missing template

--- export_button.gd
+++ export_button.gd
@@ -5,9 +5,13 @@ func _on_button_down():
 	var path = OS.get_executable_path()
 	var project_path = ProjectSettings.globalize_path("res://")
 	var template_path = "res://addons/gates-exporter-plugin/template/"
+	var export_path = project_path + "/export/project.zip"
+	OS.move_to_trash(project_path + "/export/")
 	var dir = DirAccess.open("res://")
 	dir.make_dir("export")
-	OS.execute(path, ["--headless", "--path", project_path, "--export-pack", "gates", project_path + "/export/project.zip"])
+	var exit_code = OS.execute(path, ["--headless", "--path", project_path, "--export-pack", "gates", export_path])
+	if !FileAccess.file_exists(export_path):
+		push_error("ERROR: failed to build zip, does the \"gates\" export template exist?")
 	DirAccess.copy_absolute(template_path + ".gdignore", "res://export/.gdignore") # Stops import of assets inside dir
 	DirAccess.copy_absolute(template_path + "icon.png", "res://export/icon.png")
 	DirAccess.copy_absolute(template_path + "world.gate", "res://export/world.gate")
